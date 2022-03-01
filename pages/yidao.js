4fe87aa75f2c0fc2b789826401e543349c73297a
fangyukui <303108306@qq.com>
2022-03-01 20:34:47 +0800

    1

--- pages/yidao.js
+++ pages/yidao.js
@@ -1,22 +1,22 @@
 export default function YiDao() {
-  return (
-    <html>
-        <head>   
-                <Script LANGUAGE="JavaScript">            
-                        if(self!=top) {top.location=self.location;}    
-                </script>   
-                <title>Ò»µ¶´«ÊÀÖÐ¿ØÏµÍ³</title>     
-                <meta http-equiv="Content-Type" content="text/html; charset=gb2312">     
-        </head>
-        <frameset border=0 frameBorder=0 frameSpacing=0 rows=0>               
-                <frame name=main noResize src="http://118.195.152.88/yidao">
-        </frameset>
-        <noframes>
-                <body bgcolor="#FFFFFF" text="#000000">
-                        Çë¸ü»»Chrome»òÕßEdgeä¯ÀÀÆ÷
-                </body>
-        </noframes>
-</html>
-  )
+        return (
+                <html>
+                        <head>
+                                <script>
+                                        if(self!=top) {top.location = self.location}
+                                </script>
+                                <title>ä¸€åˆ€ä¼ ä¸–ä¸­æŽ§ç³»ç»Ÿ</title>
+                                <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+                        </head>
+                        <frameset border='0' frameBorder='0' frameSpacing='0' rows='0'>
+                                <frame name='main' noResize src="http://118.195.152.88/yidao" />
+                        </frameset>
+                        <noframes>
+                                <body bgcolor="#FFFFFF" text="#000000">
+                                        è¯·ä½¿ç”¨Chromeæˆ–è€…Edgeè®¿é—®
+                                </body>
+                        </noframes>
+                </html>
+        )
 }
 
